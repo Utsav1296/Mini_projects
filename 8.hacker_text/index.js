9f88b3132d57a3aa4e5535fc8fb3a8254ff0ccb5
Utsav1296 <utsavraj1296@gmail.com>
2023-07-31 08:51:57 +0530

    adding hacker_text

---
+++ 8.hacker_text/index.js
@@ -0,0 +1,23 @@
+console.log('script added...')
+
+const letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", " ", " ", " ",];
+
+const random = Math.floor(Math.random() * 26)
+
+document.querySelector("h1").onmouseover = event => {
+   console.log("🚀 ~ file: index.js:10 ~ document.querySelector ~ innerText:", event.target.innerText)
+   let iterations = 0;
+   const interval = setInterval(() => {
+      event.target.innerText = event.target.innerText.split("").map((letter, index) => {
+         if (index < iterations && event.target.innerText.length) {
+            return event.target.dataset.value[index]
+         }
+
+         return letterArray[Math.floor(Math.random() * 26)]
+      }).join("");
+
+      if (iterations > event.target.innerText.length) clearInterval(interval);
+
+      iterations += 1;
+   }, 50);
+}
